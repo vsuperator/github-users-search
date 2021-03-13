d8312c3a84bdf151164324fc74ce9e28881f9f83
Roman Kopystyrynskiy <romankopystyrynskiy@Romans-MacBook-Pro.local>
2021-03-13 10:46:07 +0200

    Update token

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const USERS_PER_PAGE = 10;
 export const GITHUB_API_URL = "https://api.github.com/";
 export const GITHUB_URL = "https://github.com/";
-export const GITHUB_TOKEN = "06dfb85fa0a13406bcd445699992f340c9d1d1b1";
+export const GITHUB_TOKEN = "b3ce2aaee79047e036d1525e6afbff3571e5d173";
